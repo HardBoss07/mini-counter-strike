bda3ce4097f0c263ec98232de2e0cb835b541465
HardBoss07 <matteo@bosshard.net>
2026-06-18 14:57:05 +0200

    feat(backend): enable configuration properties and scheduling

--- backend/src/main/java/dev/m4tt3o/mini_cs/MiniCsApplication.java
+++ backend/src/main/java/dev/m4tt3o/mini_cs/MiniCsApplication.java
@@ -2,8 +2,12 @@ package dev.m4tt3o.mini_cs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@EnableScheduling
 public class MiniCsApplication {
 
     public static void main(String[] args) {
