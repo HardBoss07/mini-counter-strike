c0ec989fb9061d6bb2e2675d2353543cd5c7e19c
HardBoss07 <matteo@bosshard.net>
2026-06-19 09:37:59 +0200

    refactor(repo): add findByUser_Id and findByUser methods

--- backend/src/main/java/dev/m4tt3o/mini_cs/repository/LoadoutRepository.java
+++ backend/src/main/java/dev/m4tt3o/mini_cs/repository/LoadoutRepository.java
@@ -5,8 +5,10 @@ import dev.m4tt3o.mini_cs.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface LoadoutRepository extends JpaRepository<Loadout, Long> {
     Optional<Loadout> findByUserAndSide(User user, String side);
+    List<Loadout> findByUser_Id(Long userId);
 }
--- backend/src/main/java/dev/m4tt3o/mini_cs/repository/UserWeaponInstanceRepository.java
+++ backend/src/main/java/dev/m4tt3o/mini_cs/repository/UserWeaponInstanceRepository.java
@@ -1,8 +1,12 @@
 package dev.m4tt3o.mini_cs.repository;
 
+import dev.m4tt3o.mini_cs.entity.User;
 import dev.m4tt3o.mini_cs.entity.UserWeaponInstance;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
-public interface UserWeaponInstanceRepository extends JpaRepository<UserWeaponInstance, Long> {}
+public interface UserWeaponInstanceRepository extends JpaRepository<UserWeaponInstance, Long> {
+    List<UserWeaponInstance> findByUser(User user);
+}
