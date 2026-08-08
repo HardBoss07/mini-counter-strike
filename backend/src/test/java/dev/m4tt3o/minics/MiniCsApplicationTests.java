7f8d35984d7674e32d9621991c886442e4c3282b
HardBoss07 <matteo@bosshard.net>
2026-08-08 12:50:21 +0200

    dev: created dedicated testing enviorment / application-test.properties

--- backend/src/test/java/dev/m4tt3o/minics/MiniCsApplicationTests.java
+++ backend/src/test/java/dev/m4tt3o/minics/MiniCsApplicationTests.java
@@ -2,8 +2,10 @@ package dev.m4tt3o.minics;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MiniCsApplicationTests {
 
     @Test
